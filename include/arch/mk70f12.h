c0a7a5921ced2bf39c3842c87f6f5eadbad5a08b
Chris Heller <hellertime@gmail.com>
2014-04-02 21:47:27 -0400

    Add missing constants:

--- include/arch/mk70f12.h
+++ include/arch/mk70f12.h
@@ -5,6 +5,11 @@
 
 #define ENOTRECOVERABLE EINVAL
 
+#define PRIu32 "lu"
+#define PRIx32 "lx"
+#define PRIuPTR PRIu32
+#define PRIxPTR PRIx32
+
 typedef int32_t intptr_t;
 typedef uint32_t uintptr_t;
 
