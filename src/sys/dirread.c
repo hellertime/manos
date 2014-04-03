bfc0339f75522bab45ca8983fef010f929ace4a1
Chris Heller <christopherheller@fas.harvard.edu>
2014-04-02 21:30:48 -0400

    Took a gcc suggestion

--- src/sys/dirread.c
+++ src/sys/dirread.c
@@ -21,7 +21,7 @@ int dirread(int fd, NodeInfo** buf) {
         return -1;
     }
 
-    if (!p->crumb.flags & CRUMB_ISDIR) {
+    if (!(p->crumb.flags & CRUMB_ISDIR)) {
         errno = ENOTDIR;
         return -1;
     }
