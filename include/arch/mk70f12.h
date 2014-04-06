f35cec3d07744614c3f364f8f9b5a98d15a8ef07
Chris Heller <christopherheller@fas.harvard.edu>
2014-04-05 22:12:06 -0400

    Conditionalize overrides

--- include/arch/mk70f12.h
+++ include/arch/mk70f12.h
@@ -3,17 +3,41 @@
 
 #ifdef PLATFORM_K70CW
 
+#ifndef ENOBUFS
 #define ENOBUFS EINVAL
+#endif
+
+#ifndef ENODATA
 #define ENODATA EINVAL
+#endif
+
+#ifndef ENOTRECOVERABLE
 #define ENOTRECOVERABLE EINVAL
+#endif
 
+#ifndef PRIu32
 #define PRIu32 "lu"
-#define PRIx32 "lx"
-#define PRIuPTR PRIu32
-#define PRIxPTR PRIx32
+#endif
 
+#ifndef PRIx32
+#define PRIx32 "lx"
+#endif
+
+#ifndef PRIuPTR
+#define PRIuPTR PRIu32
+#endif
+
+#ifndef PRIxPTR
+#define PRIxPTR PRIx32
+#endif
+
+#ifndef intptr_t
 typedef int32_t intptr_t;
+#endif
+
+#ifndef uintptr_t
 typedef uint32_t uintptr_t;
+#endif
 
 void mcgInit(void);
 void sdramInit(void);
