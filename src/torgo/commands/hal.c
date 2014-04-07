6f8fdfbc8bf9c011e8629e91ab0faf320c1239a3
Chris Heller <christopherheller@fas.harvard.edu>
2014-04-06 21:27:15 -0400

    RLE encode image

--- src/torgo/commands/hal.c
+++ src/torgo/commands/hal.c
@@ -1,29 +1,27 @@
 #include <manos.h>
 #include <stdint.h>
 
-#define HAL_ASCII_SIZE 510476
-#define HAL_INT_SIZE 170159
-
 #include "hal2000.ascii"
 
 int cmdHal__Main(int argc, char * const argv[]) {
-    uint32_t* image = kmalloc(HAL_INT_SIZE * (sizeof *image));
-    if (!image) {
-        fprintln(u->tty, "Unable to allocate memory for image conversion");
-        return 1;
-    }
+    char blitbuf[LCD_SDRAM_SIZE] = {0};
 
-    for (unsigned i = 0, j = 0; i < HAL_ASCII_SIZE; i+= 3, j++) {
-        uint32_t pixel = *(uint32_t*)&hal[i]; /* this stores 4 bytes, but hal only holds 3 octets per pixel */
-        pixel >>= 8; /* so shift out the last pixel value */
-        image[j] = pixel;
+    char* p = &blitbuf[0];
+    size_t bytes = 0;
+    for (uint32_t i = 0; i < COUNT_OF(hal); i += 2) {
+        uint32_t octet = hal[i];
+        uint32_t run = hal[i + 1];
+
+        while (run--) {
+            *p++ = (uint8_t)octet;
+            bytes++;
+        }
     }
 
     int fd = kopen("/dev/lcd/blit", CAP_WRITE);
-    kwrite(fd, image, HAL_INT_SIZE * (sizeof *image));
+    kwrite(fd, blitbuf, bytes);
     kclose(fd);
 
-    kfree(image);
     UNUSED(argv);
     UNUSED(argc);
     return 0;
