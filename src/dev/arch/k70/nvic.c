7387f382531ec7cbb1551ab69b137344ab4c4045
Chris Heller <christopherheller@fas.harvard.edu>
2014-04-15 21:08:40 -0400

    Fixed interrupt priority inversion

--- src/dev/arch/k70/nvic.c
+++ src/dev/arch/k70/nvic.c
@@ -12,7 +12,7 @@ void enableNvicIrq(unsigned irq, uint8_t priority) {
 
     /* K70 reference manual (p90) states that the ISER reg num for an IRQ is found with IRQ div 32 */
     /* the manual goes on to state (p91) that to locate a bit field in a register the offset is irq mod 32 */
-    NVIC_ISER_REG(NVIC_BASE_PTR, irq >> 5) = NVIC_ISER_SETENA(1 << (irq % 32));
+    NVIC_ISER_REG(NVIC_BASE_PTR, irq / 32) = NVIC_ISER_SETENA(1 << (irq % 32));
 
     /* for a given IRQ, the high 4 bits of its 8bit field hold the priority level */
     NVIC_IP_REG(NVIC_BASE_PTR, irq) = (priority & 0xf) << 4;
