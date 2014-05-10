9868671ad597fb361dab4b92806b81ae291b0982
Chris Heller <christopherheller@fas.harvard.edu>
2014-05-09 23:39:22 -0400

    Forget it

--- src/dev/devswpb.c
+++ src/dev/devswpb.c
@@ -45,11 +45,7 @@ __asm(
 		"    adds r0,r0,#-1\n\t"
 		"    bne  nanosleep\n\t"
 		"    bx   lr"
-                :
-                :
-                : "r0"
      );
-}
 #else
 void nanosleep(unsigned long int nanos) {
     while(nanos-->0)
