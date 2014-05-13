4ffa3877cd6be7e7107fc9e0e2b13684b0e57bcc
Chris Heller <christopherheller@fas.harvard.edu>
2014-05-13 16:29:18 -0400

    Scale out

--- src/torgo/commands/fizzbuzz.c
+++ src/torgo/commands/fizzbuzz.c
@@ -7,13 +7,13 @@ int cmdFizzbuzz__Main(int argc, char * const argv[]) {
     int buzz = kopen("/dev/led/blue", CAP_WRITE);
 
     for (long i = 0; ; i++) {
-        if (i % 500 == 0 && i % 300 == 0) {
+        if (i % 50000 == 0 && i % 30000 == 0) {
             kwrite(fizz, "1", 1);
             kwrite(buzz, "1", 1);
-        } else if (i % 500 == 0) {
+        } else if (i % 50000 == 0) {
             kwrite(fizz, "0", 1);
             kwrite(buzz, "1", 1);
-        } else if (i % 300 == 0) {
+        } else if (i % 30000 == 0) {
             kwrite(buzz, "0", 1);
             kwrite(fizz, "1", 1);
         }
