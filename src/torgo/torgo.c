27bb66beed82f16942985bd91f4271de2d34dd65
Chris Heller <christopherheller@fas.harvard.edu>
2014-05-13 16:14:20 -0400

    DOn't strlen no str

--- src/torgo/torgo.c
+++ src/torgo/torgo.c
@@ -168,10 +168,12 @@ int populateCmdArgsShell(Env *env, ParseResult *result, int *argc, char ***argv)
     memcpy(argv_[i], arg, strlen(arg));
   }
 
-  char* lastArg = argv_[argc_ - 1];
-  if (*(lastArg + strlen(lastArg)) == '&') {
-    *(lastArg + strlen(lastArg)) = 0;
-    bg = 1;
+  if (argc_) {
+     char* lastArg = argv_[argc_ - 1];
+     if (*(lastArg + strlen(lastArg)) == '&') {
+         *(lastArg + strlen(lastArg)) = 0;
+        bg = 1;
+     }
   }
 
   freeCharBuf(tokenBuilder);
