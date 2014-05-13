f6de455726f6155ef6efc7a4c3075b253b5495fa
Chris Heller <christopherheller@fas.harvard.edu>
2014-05-13 16:25:29 -0400

    Please work

--- src/torgo/commands/fizzbuzz.c
+++ src/torgo/commands/fizzbuzz.c
@@ -7,16 +7,15 @@ int cmdFizzbuzz__Main(int argc, char * const argv[]) {
     int buzz = kopen("/dev/led/blue", CAP_WRITE);
 
     for (long i = 0; ; i++) {
-        if (i % 5 == 0 && i % 3 == 0) {
+        if (i % 500 == 0 && i % 300 == 0) {
             kwrite(fizz, "1", 1);
             kwrite(buzz, "1", 1);
-        } else if (i % 5 == 0) {
-            kwrite(buzz, "1", 1);
-        } else if (i % 3 == 0) {
-            kwrite(fizz, "1", 1);
-        } else {
+        } else if (i % 500 == 0) {
             kwrite(fizz, "0", 1);
+            kwrite(buzz, "1", 1);
+        } else if (i % 300 == 0) {
             kwrite(buzz, "0", 1);
+            kwrite(fizz, "1", 1);
         }
     }
 }
